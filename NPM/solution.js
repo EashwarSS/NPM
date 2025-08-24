e14303a5d6168aa3bfdc6de2429a827da05d43da
Kadavala Eashwar Sundhar Sai <eashwarlucky.com>
2025-08-25 01:24:08 +0530

    NPM

---
+++ NPM/solution.js
@@ -0,0 +1,16 @@
+// var generateName = require("sillyname");
+// import generateName from "sillyName";
+
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+// const superheroes = require("superheroes");
+// const superheroes = require('superheroes');
+
+// const name = superheroes.random();
+// console.log(`I am ${name}!`);
+
+import { randomSuperhero } from 'superheroes';
+
+console.log(`I am ${randomSuperhero()}!`);
